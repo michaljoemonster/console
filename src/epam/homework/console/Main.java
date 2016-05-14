c0c51d1e25076cae84897186cb5ccb90e4050ef8
MichalWarjas <warjasmichal@gmail.com>
2016-05-14 15:25:19 +0200

    CheckInput method implementation
    Handling different types of possible input provider by user. I decided
    to divide method for this purpose to make maintenance of the code
    easier. It is quite probably that i forgot about something at this
    moment

--- src/epam/homework/console/Main.java
+++ src/epam/homework/console/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	private static final String myShellSequence ="[My Shell]";
 	private static String myConsoleVariableSequence = "$";
 	private static final String sharpBracket = ">";
+	private static String cdParameter;
 	public static void main(String[] args) {
 
 		try (Scanner in = new Scanner (System.in,"UTF-8"))
@@ -24,7 +25,41 @@ public class Main {
 	}
 	private static String CheckInput(String line)
 	{
-		return "";
+		if (line.length() < 1) return "justEnter";
+		String command =  new String("");
+		if (line.indexOf(" ") >= 0)command = line.substring(0, line.indexOf(" "));
+		else command = line;
+			command.toLowerCase(); // there was no explicit statement about possible letters case in the exercise description
+			switch(command)
+			{
+			case "prompt":
+				String promptParameter = line.substring(7);
+					promptParameter.trim();
+					
+					switch(promptParameter)
+					{
+					case "reset":
+						return "reset";
+					case "$cwd":
+						return "currentDir";
+					}
+					return "repeat";
+			case "exit":
+				return "exit";
+			case "dir":
+				return "dir";
+			case "tree":
+				return "tree";
+			case"cd":
+				if (line.length()<4) return "brakKatalogu";
+				cdParameter = line.substring(3);
+				cdParameter.trim();
+		//		cdParameter.toLowerCase();
+				
+			if (cdParameter.equals(".."))return "getRootDir";
+			else return "getSubDir";
+	}
+			return "unknown";
 	}
 	private static void PrepareOutput(String line, String command)
 	{
