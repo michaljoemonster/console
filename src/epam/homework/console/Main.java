1f6ab167cc1f6c70bb3dadc85fc11caf6950b062
MichalWarjas <warjasmichal@gmail.com>
2016-05-14 15:03:47 +0200

    Adding main logic
    I prepared custom command prompt and implemented  main logic framework
    (first checking the string line that user has input then preparing
    suitable output)

--- src/epam/homework/console/Main.java
+++ src/epam/homework/console/Main.java
@@ -1,10 +1,33 @@
 package epam.homework.console;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	private static String command;
+	private static final String myShellSequence ="[My Shell]";
+	private static String myConsoleVariableSequence = "$";
+	private static final String sharpBracket = ">";
 	public static void main(String[] args) {
 
-
+		try (Scanner in = new Scanner (System.in,"UTF-8"))
+		{
+		System.out.print(myShellSequence + " " + myConsoleVariableSequence + sharpBracket);
+		
+		while (true)
+		{
+			String line = in.nextLine();
+		command = CheckInput(line);
+		System.out.println(command); // for testing actual command
+		PrepareOutput(line, command);
+		}
+		}
 	}
-
+	private static String CheckInput(String line)
+	{
+		return "";
+	}
+	private static void PrepareOutput(String line, String command)
+	{
+		
+	}
 }
